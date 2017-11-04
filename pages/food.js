10c87c7f5728fb46393e88ab287a614853036b54
vire <code.vire@gmail.com>
2017-11-04 12:51:16 +0100

    lazy load image

--- pages/food.js
+++ pages/food.js
@@ -23,23 +23,172 @@ const FoodPage = () => {
         <div className="c-card c-card--accordion u-high">
           <input type="checkbox" id="accordion-1" />
           <label className="c-card__item" htmlFor="accordion-1">
-            Order breakfast - Cantina
+            Order breakfast from <span style={{ textDecoration: 'underline' }}>Canteen</span>
           </label>
           <div className="c-card__item">
-            Pane 1
-            <p className="c-paragraph">This is an example paragraph</p>
+            <ul className="c-list c-list--unstyled">
+              <li className="c-list__item FoodPage__item">
+                <span role="img" aria-label="images">
+                  {' '}
+                  🍳🥓
+                </span>
+                <span>English breakfast</span>
+                <div className="c-input-group c-input-group--rounded FoodPage__amount-box">
+                  <button className="c-button c-button--brand">-</button>
+                  <div className="o-field">
+                    <input className="c-field" placeholder="0" type="number" />
+                  </div>
+                  <button className="c-button c-button--brand">+</button>
+                </div>
+              </li>
+              <li className="c-list__item FoodPage__item">
+                <span role="img" aria-label="images">
+                  {' '}
+                  🥐☕️
+                </span>
+                <span>Croissant + Coffee</span>
+                <div className="c-input-group c-input-group--rounded FoodPage__amount-box">
+                  <button className="c-button c-button--brand">-</button>
+                  <div className="o-field">
+                    <input className="c-field" placeholder="0" type="number" />
+                  </div>
+                  <button className="c-button c-button--brand">+</button>
+                </div>
+              </li>{' '}
+              <li className="c-list__item FoodPage__item">
+                <span role="img" aria-label="images">
+                  {' '}
+                  🍚🍓
+                </span>
+                <span>Porridge with fruits</span>
+                <div className="c-input-group c-input-group--rounded FoodPage__amount-box">
+                  <button className="c-button c-button--brand">-</button>
+                  <div className="o-field">
+                    <input className="c-field" placeholder="0" type="number" />
+                  </div>
+                  <button className="c-button c-button--brand">+</button>
+                </div>
+              </li>
+            </ul>
+            <div style={{ display: 'flex' }}>
+              <label className="c-field c-field--choice">
+                <input name="radios" type="radio" checked /> I will pickup
+              </label>
+              <label className="c-field c-field--choice">
+                <input name="radios" type="radio" /> You deliver
+              </label>
+            </div>
+            <div style={{ paddingTop: '10px' }} />
+            <button type="button" className="c-button c-button--block c-button--info">
+              Order
+            </button>
           </div>
+          {/* 2nd section*/}
           <input type="checkbox" id="accordion-2" />
           <label className="c-card__item" htmlFor="accordion-2">
-            Order some snacks - Buffet
+            Order snacks from <span style={{ textDecoration: 'underline' }}>Snack bar</span>
           </label>
           <div className="c-card__item">
-            Pane 2
-            <p className="c-paragraph">This is another example paragraph</p>
+            <ul className="c-list c-list--unstyled">
+              <li className="c-list__item FoodPage__item">
+                <span role="img" aria-label="images">
+                  {' '}
+                  🍎🍐
+                </span>
+                <span>Small fruit salad</span>
+                <div className="c-input-group c-input-group--rounded FoodPage__amount-box">
+                  <button className="c-button c-button--brand">-</button>
+                  <div className="o-field">
+                    <input className="c-field" placeholder="0" type="number" />
+                  </div>
+                  <button className="c-button c-button--brand">+</button>
+                </div>
+              </li>
+              <li className="c-list__item FoodPage__item">
+                <span role="img" aria-label="images">
+                  {' '}
+                  🍉🍇🍍
+                </span>
+                <span>Large fruit salad</span>
+                <div className="c-input-group c-input-group--rounded FoodPage__amount-box">
+                  <button className="c-button c-button--brand">-</button>
+                  <div className="o-field">
+                    <input className="c-field" placeholder="0" type="number" />
+                  </div>
+                  <button className="c-button c-button--brand">+</button>
+                </div>
+              </li>{' '}
+              <li className="c-list__item FoodPage__item">
+                <span role="img" aria-label="images">
+                  {' '}
+                  🍰
+                </span>
+                <span>Cake</span>
+                <div className="c-input-group c-input-group--rounded FoodPage__amount-box">
+                  <button className="c-button c-button--brand">-</button>
+                  <div className="o-field">
+                    <input className="c-field" placeholder="0" type="number" />
+                  </div>
+                  <button className="c-button c-button--brand">+</button>
+                </div>
+              </li>
+            </ul>
+            <div style={{ display: 'flex' }}>
+              <label className="c-field c-field--choice">
+                <input name="radios" type="radio" checked /> I will pickup
+              </label>
+              <label className="c-field c-field--choice">
+                <input name="radios" type="radio" /> You deliver
+              </label>
+            </div>
+            <div style={{ paddingTop: '10px' }} />
+            <button type="button" className="c-button c-button--block c-button--info">
+              Order
+            </button>
+          </div>
+          {/* 3rd section */}
+          <input type="checkbox" id="accordion-3" />
+          <label className="c-card__item" htmlFor="accordion-3">
+            Request food from <span style={{ textDecoration: 'underline' }}>Damejidlo</span>
+          </label>
+          <div className="c-card__item">
+            <small>Specify food type (pizza), amount, preferred restaurant, delivery time </small>
+            <textarea className="c-field" placeholder="Type in here..." />
+            <div style={{ paddingTop: '10px' }} />
+            <button type="button" className="c-button c-button--block c-button--info">
+              Request
+            </button>
+          </div>
+          {/* 4th section */}
+          <input type="checkbox" id="accordion-4" />
+          <label className="c-card__item" htmlFor="accordion-4">
+            Join random lunch
+          </label>
+          <div className="c-card__item">
+            <div style={{ textAlign: 'center' }}>
+              <span role="img" aria-label="images" style={{ fontSize: '64px' }}>
+                🎲
+              </span>
+            </div>
+            <div style={{ paddingTop: '10px' }} />
+            <button type="button" className="c-button c-button--block c-button--info">
+              Join
+            </button>
           </div>
         </div>
       </div>
       <style jsx>{`
+        .FoodPage__item {
+          padding: 10px 0px;
+          display: flex;
+          flex-direction: row;
+          justify-content: space-between;
+        }
+
+        .FoodPage__amount-box {
+          max-width: 90px;
+        }
+
         .HomePage__item {
           width: 140px;
           text-align: center;
--- pages/navigation.js
+++ pages/navigation.js
@@ -1,12 +1,78 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
 import Layout from '../components/Layout'
 
-const NavPage = () => {
-  return (
-    <Layout>
-      <img alt="img" src="https://i.imgur.com/cW2HBu0.png" />
-    </Layout>
-  )
+const OverlayImg = ({ dummyUrl, realUrl, overlay, handleClick }) => (
+  <div>
+    {overlay && (
+      <div
+        role="presentation"
+        onClick={handleClick}
+        onKeyDown={handleClick}
+        className="NavPage__overlay">
+        <div className="NavPage__overlay-text">CLICK</div>
+      </div>
+    )}
+
+    {overlay ? (
+      <img alt="img" style={{ width: '100%' }} src={dummyUrl} />
+    ) : (
+      <img alt="img" style={{ width: '100%' }} src={realUrl} />
+    )}
+    <style jsx>{`
+      .NavPage__overlay {
+        width: 100%;
+        background: rgba(150, 0, 255, 0.43);
+        z-index: 1000;
+        position: absolute;
+        height: 100%;
+      }
+
+      .NavPage__overlay-text {
+        position: relative;
+        top: 40vh;
+        text-align: center;
+        font-size: 90px;
+        font-weight: bold;
+        color: white;
+      }
+    `}</style>
+  </div>
+)
+
+OverlayImg.propTypes = {
+  dummyUrl: PropTypes.string.isRequired,
+  realUrl: PropTypes.string.isRequired,
+  overlay: PropTypes.bool.isRequired,
+  handleClick: PropTypes.func.isRequired,
+}
+
+class NavPage extends React.Component {
+  state = {
+    showOverlay: true,
+  }
+  handleClick = () => {
+    this.setState({
+      showOverlay: false,
+    })
+  }
+  render() {
+    const dummySrc = 'https://i.imgur.com/VY2vgSQ.jpg'
+    const imgSrc = 'https://i.imgur.com/cW2HBu0.png'
+    return (
+      <Layout>
+        <div className="NavPage__content" style={{ background: '#66e8d0', height: '100vh' }}>
+          <OverlayImg
+            overlay={this.state.showOverlay}
+            handleClick={this.handleClick}
+            dummyUrl={dummySrc}
+            realUrl={imgSrc}
+          />
+        </div>
+      </Layout>
+    )
+  }
 }
 
 export default NavPage
